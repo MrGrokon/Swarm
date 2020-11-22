97532b52dce2d32804a765093d7460e50416fa0f
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2020-11-22 14:58:36 +0100

    Modification du SwarmManager
    Nouvelle fonction de GetCenterOfMass(vect3), GetLeaderDynamicly(vect3, int =3).
    Ajout d'un paramettre à GetAllCreatures(float), si positif, il ne revenvoie que les creatures dont la distance par rapport au leader et inférieur au param

--- Assets/Scripts/SwarmBehavior/CreatureManager.cs
+++ Assets/Scripts/SwarmBehavior/CreatureManager.cs
@@ -11,6 +11,8 @@ public class CreatureManager : MonoBehaviour
     [SerializeField]
     private List<GameObject> SwarmCreatures = new List<GameObject>();
 
+    public float MaxSwarmDistanceToGetMedian = 5f;
+
     private void Awake()
     {
         #region Singleton Instance
@@ -42,21 +44,52 @@ public class CreatureManager : MonoBehaviour
         }
     }
 
-    public GameObject[] GetAllCreatures()
+    public GameObject[] GetAllCreatures(float OptionalMaxDistance = -1f)
     {
+        if(OptionalMaxDistance >0f){
+            List<GameObject> TempTab = new List<GameObject>();
+            foreach(var crea in SwarmCreatures){
+                //le referenciel du calcul ne devrait pas forcement etre la MasterCreature, mais je ne vois pas de meilleur facon de le gerer de suite
+                if(Vector3.Distance(MasterCreature.transform.position, crea.transform.position) <= OptionalMaxDistance){
+                    TempTab.Add(crea.gameObject);
+                }
+            }
+            return TempTab.ToArray();
+        }
         return SwarmCreatures.ToArray();
     }
     #endregion
 
     #region Creature Master Related
-       /*public void GetCenterOfMass()
+       public Vector3 GetCenterOfMass()
        {
+           Vector3 CenterOfMass = Vector3.zero;
+           CenterOfMass += GetLimitOnAxis(Vector3.right, true);
+           CenterOfMass += GetLimitOnAxis(Vector3.right, false);
+           CenterOfMass += GetLimitOnAxis(Vector3.up, true);
+           CenterOfMass += GetLimitOnAxis(Vector3.up, false);
+           CenterOfMass += GetLimitOnAxis(Vector3.forward, true);
+           CenterOfMass += GetLimitOnAxis(Vector3.forward, false);
 
-       }*/
+           CenterOfMass = CenterOfMass/6;
+           return CenterOfMass;
+       }
+
+       public GameObject GetLeaderDynamically(Vector3 JoystickDirection, int NmbOfCellAveraged = 3){
+           GameObject[] Cells_orderedByAngle = GetAllCreatures(MaxSwarmDistanceToGetMedian).OrderBy(x => Vector3.Angle(JoystickDirection, x.transform.position - GetCenterOfMass())).ToArray<GameObject>();
+            GameObject FarestCellOfAngle = Cells_orderedByAngle[0];
+            for (int i = 0; i < NmbOfCellAveraged; i++)
+            {
+                if(Vector3.Distance(GetCenterOfMass(), Cells_orderedByAngle[i].transform.position) > Vector3.Distance(GetCenterOfMass(), FarestCellOfAngle.transform.position)){
+                    FarestCellOfAngle = Cells_orderedByAngle[i];
+                }
+            }
+            return FarestCellOfAngle;
+       }
     #endregion
 
     #region Low Level Functions
-    /*private Vector3 GetLimitOnAxis(Vector3 axis, bool _max)
+    private Vector3 GetLimitOnAxis(Vector3 axis, bool _max)
     {
         //return the highest or lower cell position on a defined axis
         if (Application.isPlaying == true)
@@ -64,7 +97,7 @@ public class CreatureManager : MonoBehaviour
             if (axis == Vector3.right || axis == Vector3.left)
             {
                 //axis == X
-                GameObject[] Ordered_Creatures = GetAllCreatures().OrderBy(_crea => _crea.transform.position.x).ToArray();
+                GameObject[] Ordered_Creatures = GetAllCreatures(MaxSwarmDistanceToGetMedian).OrderBy(_crea => _crea.transform.position.x).ToArray();
                 if (_max)
                 {
                     int MaxArraySize = Ordered_Creatures.Length;
@@ -78,7 +111,7 @@ public class CreatureManager : MonoBehaviour
             else if (axis == Vector3.up || axis == Vector3.down)
             {
                 //axis == Y
-                GameObject[] OrderedCreatures = GetAllCreatures().OrderBy(_crea => _crea.transform.position.y).ToArray();
+                GameObject[] OrderedCreatures = GetAllCreatures(MaxSwarmDistanceToGetMedian).OrderBy(_crea => _crea.transform.position.y).ToArray();
                 if (_max)
                 {
                     int MaxArraySize = OrderedCreatures.Length;
@@ -92,7 +125,7 @@ public class CreatureManager : MonoBehaviour
             else if (axis == Vector3.forward || axis == Vector3.back)
             {
                 //axis == Z
-                GameObject[] Ordered_Creatures = GetAllCreatures().OrderBy(_crea => _crea.transform.position.z).ToArray();
+                GameObject[] Ordered_Creatures = GetAllCreatures(MaxSwarmDistanceToGetMedian).OrderBy(_crea => _crea.transform.position.z).ToArray();
                 if (_max)
                 {
                     int MaxArraySize = Ordered_Creatures.Length;
@@ -105,6 +138,6 @@ public class CreatureManager : MonoBehaviour
             }
         }
         return Vector3.zero;
-    }*/
+    }
     #endregion
 }
