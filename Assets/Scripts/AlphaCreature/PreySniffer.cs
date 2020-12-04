f79ca8f7def058464de6d2f2e02694d33c6f1021
Théo Pinto <decratosgames@gmail.com>
2020-12-04 19:09:15 +0100

    Reformation du maib

---
+++ Assets/Scripts/AlphaCreature/PreySniffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreySniffer : MonoBehaviour
+{
+    public GameObject proof;
+    private GameObject target;
+
+    private void GetPrey()
+    {
+        target = proof.GetComponent<DetectPlayer>().prey;
+    }
+
+    private void LateUpdate()
+    {
+        if (InputTester.inputInstance._playerInputs.Actions.GetPrey.ReadValue<float>() != 0)
+        {
+            GetPrey();
+        }
+    }
+}
