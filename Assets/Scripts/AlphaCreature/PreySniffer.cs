053bce61322e7aff370c6a2c4517cb3b2a7a84df
Théo Pinto <decratosgames@gmail.com>
2020-12-05 00:20:28 +0100

    Ajout de matériaux debugs, Ajout d'IA basique sur la proie, Création d'un petit terrain

--- Assets/Scripts/AlphaCreature/PreySniffer.cs
+++ Assets/Scripts/AlphaCreature/PreySniffer.cs
@@ -23,18 +23,25 @@ public class PreySniffer : MonoBehaviour
     {
         if (InputTester.inputInstance._playerInputs.Actions.GetPrey.ReadValue<float>() != 0 && !inputTriggered)
         {
+            trail.gameObject.SetActive(true);
             GetPrey();
             inputTriggered = true;
         }
 
-        if (OnHunt)
+        if (OnHunt && target)
         {
             trail.SetPosition(0, Objects.Instance.Alpha.transform.position);
             trail.SetPosition(1, target.transform.position);
             if (InputTester.inputInstance._playerInputs.Actions.GetPrey.ReadValue<float>() != 0 && !inputTriggered)
             {
                 OnHunt = false;
+                trail.gameObject.SetActive(false);
             }
+        }
+        else
+        {
+            OnHunt = false;
+            trail.gameObject.SetActive(false);
         }
 
         if (InputTester.inputInstance._playerInputs.Actions.GetPrey.ReadValue<float>() == 0 && inputTriggered)
