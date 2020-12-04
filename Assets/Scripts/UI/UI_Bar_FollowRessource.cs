2c185f6dafe93ea61744b614e8c75c80ba1ca949
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2020-12-04 20:47:35 +0100

    Mise en Place des systeme de santé et nouriture Ainsi que leurs UI

---
+++ Assets/Scripts/UI/UI_Bar_FollowRessource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Bar_FollowRessource : MonoBehaviour
+{
+    public RessourceManager.Resource Rsc_Followed;
+    private Slider _slider;
+    
+    #region Unity Functions
+        private void Awake() {
+            _slider = this.GetComponent<Slider>();
+            if(_slider == null){
+                Debug.Log("Slider Not Found: must be place on a UI Object");
+            }
+            else{
+                //_slider.maxValue = 
+            }
+            
+        }
+
+        private void Update() {
+            //_slider.value = Alpha.getressource(type)
+        }
+    #endregion
+}
