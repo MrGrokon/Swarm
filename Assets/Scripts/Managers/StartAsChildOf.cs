6da671c417739d64fdaa54c54db33a6f1fc64ebc
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2020-12-06 17:02:04 +0100

    GenerateBetaOnStart fonctionnel et Tweakable + Ajout d'un script général  StartAsChildOf() + Modif de map

---
+++ Assets/Scripts/Managers/StartAsChildOf.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAsChildOf : MonoBehaviour
+{
+    public string ParentName;
+    private void Awake() {
+        GameObject Parent = GameObject.Find(ParentName);
+        if(Parent != null || this.transform.parent != Parent){
+            this.transform.SetParent(Parent.transform);
+        }
+        else{
+        	Debug.Log("Probleme With StartAsChildOf");
+        }
+    }
+}
