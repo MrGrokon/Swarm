654529b1e32a5da69e2c5492fcf5cc8a61237b94
Théo Pinto <decratosgames@gmail.com>
2020-12-14 12:03:06 +0100

    0.5.1.1 : Ajout du sound detection

---
+++ Assets/Scripts/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+[CustomEditor(typeof(AiDetection))]
+public class FieldOfViewEditor : Editor
+{
+   private void OnSceneGUI()
+   {
+      AiDetection AD = (AiDetection)target;
+      Handles.color = Color.white;
+      Handles.DrawWireArc(AD.transform.position, Vector3.up, Vector3.forward, 360, AD.viewRadius);
+      Vector3 viewAngleA = AD.DirFromAngle(-AD.viewAngle / 2, false);
+      Vector3 viewAngleB = AD.DirFromAngle(AD.viewAngle / 2, false);
+      
+      Handles.DrawLine(AD.transform.position, AD.transform.position + viewAngleA * AD.viewRadius);
+      Handles.DrawLine(AD.transform.position, AD.transform.position + viewAngleB * AD.viewRadius);
+   }
+}
---
+++ Assets/Scripts/Editor/SoundRangeEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(AiSoundDetection))]
+public class SoundRangeEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        AiSoundDetection AD = (AiSoundDetection)target;
+        Handles.color = Color.red;
+        Handles.DrawWireArc(AD.transform.position, Vector3.up, Vector3.forward, 360, AD.viewRadius);
+        Vector3 viewAngleA = AD.DirFromAngle(-AD.viewAngle / 2, false);
+        Vector3 viewAngleB = AD.DirFromAngle(AD.viewAngle / 2, false);
+      
+        Handles.DrawLine(AD.transform.position, AD.transform.position + viewAngleA * AD.viewRadius);
+        Handles.DrawLine(AD.transform.position, AD.transform.position + viewAngleB * AD.viewRadius);
+    }
+}
