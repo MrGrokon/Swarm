b882d0524cc5da0c8f44b2bdc6bb43479636614d
Théo Pinto <decratosgames@gmail.com>
2020-12-15 15:33:15 +0100

    0.5.2.0 : Changement de la caméra 3ème personne

---
+++ Assets/TrackInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackInfo : MonoBehaviour
+{
+    [Header("Global parameters")]
+    [SerializeField] private float[] anglesByFreshnessLevel;
+    [SerializeField] private float[] distanceByFreshnessLevel;
+
+    [Header("Local parameters")]
+    public float actualAngle;
+
+    public float actualDistance;
+
+    
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)  //Créer une direction depuis un angle, uniquement utilisée dans les scripts éditeur
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(actualAngle * Mathf.Deg2Rad), 0,Mathf.Cos(actualAngle * Mathf.Deg2Rad));
+    }
+}
