8496daf51ec6d009d8bfdbb24129a18e4dbebaed
Arcanis <bastienbarth@gmail.com>
2021-01-07 19:39:35 +0100

    Revert "Revert "1.1.4.1 : Ajout du player start, Fix partiel des IA qui font du sur place, Création d'un player group""
    This reverts commit 39c4c1593396cc4c23b6fae135ce1b2c701da41e.

---
+++ Assets/PlayerStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStart : MonoBehaviour
+{
+    [SerializeField] private GameObject playerGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerGroup, transform.position, Quaternion.identity);
+    }
+
+}
