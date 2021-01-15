db8ce3ce144b7102ea5d7cf9b38dfdfb0bea6764
Théo Pinto <decratosgames@gmail.com>
2021-01-16 00:57:41 +0100

    [1.1.4.7] Modification et amélioration du spawner

--- Assets/Scripts/Managers/GeneratePrey.cs
+++ Assets/Scripts/Managers/GeneratePrey.cs
@@ -1,39 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class GeneratePrey : MonoBehaviour
 {
 
     [SerializeField] private GameObject terrain;
 
-    private Vector2 minMaxX;
+    [SerializeField] private Vector2 minMaxX;
 
-    private Vector2 minMaxY;
+    [SerializeField] private Vector2 minMaxY;
 
     [SerializeField] private int maxPrey;
 
     [SerializeField] public List<GameObject> listOfPrey;
 
     [SerializeField] private GameObject preyPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        minMaxX = new Vector2(-terrain.transform.localScale.x, terrain.transform.localScale.x);
-        minMaxY = new Vector2(-terrain.transform.localScale.z, terrain.transform.localScale.z);
-    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (listOfPrey.Count < maxPrey)  //Génération des proies
         {
-            Vector3 randomSpawnPosition = Random.insideUnitSphere * 200f;
+            float X = Random.Range(minMaxX.x, minMaxX.y);
+            float Y = Random.Range(minMaxY.x, minMaxY.y);
             NavMeshHit hit;
-            NavMesh.SamplePosition(randomSpawnPosition, out hit, 200f, NavMesh.AllAreas); //Sample la position donnée sur le navmesh et peut retourner un bool
+            NavMesh.SamplePosition(new Vector3(X,terrain.transform.position.y, Y), out hit, 200f, NavMesh.AllAreas); //Sample la position donnée sur le navmesh et peut retourner un bool
             var prey = Instantiate(preyPrefab, hit.position, Quaternion.identity);
             listOfPrey.Add(prey);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position - new Vector3(minMaxX.x,0, minMaxY.y), 1f);
+        Gizmos.DrawSphere(transform.position - new Vector3(minMaxX.y,0, minMaxY.y), 1f);
+        Gizmos.DrawSphere( transform.position - new Vector3(minMaxX.x,0, minMaxY.x), 1f);
+        Gizmos.DrawSphere(transform.position - new Vector3(minMaxX.y,0, minMaxY.x), 1f);
     }
 }
